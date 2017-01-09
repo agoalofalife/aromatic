96f8677febe860ef081eb9f79c688c9af1f5c9f6
agoalofalife <agoalofalife@gmail.com>
2017-01-09 20:25:43 +0300

    add webpack file

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    entry: "./index.ts",
+    output: {
+        filename: "./index.js",
+    },
+
+    // Включить карты кода для отладки вывода webpack
+    devtool: "source-map",
+
+    resolve: {
+        // Добавить разрешения '.ts' и '.tsx' к обрабатываемым
+        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    },
+    watch : true,
+    watchOptions : {
+        aggregateTimeout : 100
+    },
+    module: {
+        loaders: [
+            // Все файлы с разрешениями '.ts' или '.tsx' будет обрабатывать 'ts-loader'
+            { test: /\.tsx?$/, loader: "ts-loader" }
+        ],
+
+        preLoaders: [
+            // Все карты кода для выходных '.js'-файлов будет дополнительно обрабатывать `source-map-loader`
+            { test: /\.js$/, loader: "source-map-loader" }
+        ]
+    }
+};
