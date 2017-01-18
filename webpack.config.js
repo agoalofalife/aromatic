7742f0289a0bb677def22cf9ea7183e4558f9fa2
agoalofalife <agoalofalife@gmail.com>
2017-01-18 16:48:30 +0300

    cleaner

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,9 @@ module.exports = {
         filename: "./index.js",
     },
 
-    // Включить карты кода для отладки вывода webpack
     devtool: "source-map",
 
     resolve: {
-        // Добавить разрешения '.ts' и '.tsx' к обрабатываемым
         extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
     },
     watch : true,
@@ -17,12 +15,10 @@ module.exports = {
     },
     module: {
         loaders: [
-            // Все файлы с разрешениями '.ts' или '.tsx' будет обрабатывать 'ts-loader'
             { test: /\.tsx?$/, loader: "ts-loader" }
         ],
 
         preLoaders: [
-            // Все карты кода для выходных '.js'-файлов будет дополнительно обрабатывать `source-map-loader`
             { test: /\.js$/, loader: "source-map-loader" }
         ]
     }
